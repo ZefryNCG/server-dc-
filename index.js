22a0a76d63a32c640789b1f9ab0e761ca29be8da
ZEFRY NGACHENG <161859102+ZefryNCG@users.noreply.github.com>
2024-08-16 10:57:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ require('./cmd')(client);
 require('./sharecmd')(client);
 require('./server')(client);
 
-client.login('MTI2Njc3Mzk5ODE3MjExMDkxOQ.GRFcY3.jnvt6zxKOgWq7fnBTqtO3SGmtCCXbodejm9XMA'); // Ganti dengan token bot Anda yang benar
+const token = process.env.TOKEN;
+
+client.login(token);
